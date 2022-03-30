c9f0eece7db8e153b1377269ffadf4f35b241b78
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-03-30 13:41:14 -0600

    empece con la base de datos

--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    # tabla de usuarios
+    nombre_usuario = models.CharField(max_length=200)
+    pais = models.CharField(max_length=200)
+    fecha_nacimiento = models.DateField()
+    '''
+    "id_usuario"	INTEGER,
+	"Nombre_de_usuario"	TEXT NOT NULL, YA
+	"Pais"	TEXT NOT NULL,
+	"Fecha_de_nacimiento"	TEXT NOT NULL,
+	"Contraseña"	TEXT NOT NULL,
+	"Correo"	TEXT NOT NULL,
+	"Apellido_materno"	TEXT NOT NULL,
+	"Apellido_paterno"	TEXT NOT NULL,
+	"Genero"	TEXT NOT NULL,
+	"Telefono"	TEXT NOT NULL,
+	"Foto"	TEXT NOT NULL,
+	"Status"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id_usuario")
+    '''
