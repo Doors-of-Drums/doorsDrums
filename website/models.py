ecdf3dd40a6cb0c9e96c35228118202ec0e60f24
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-03-30 14:53:38 -0600

    creation of the database

--- website/models.py
+++ website/models.py
@@ -2,23 +2,35 @@ from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
 class Usuario(models.Model):
     # tabla de usuarios
-    nombre_usuario = models.CharField(max_length=200)
-    pais = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
+    nombre_usuario = models.CharField(max_length=50)
+    pais = models.CharField(max_length=100)
     fecha_nacimiento = models.DateField()
-    '''
-    "id_usuario"	INTEGER,
-	"Nombre_de_usuario"	TEXT NOT NULL, YA
-	"Pais"	TEXT NOT NULL,
-	"Fecha_de_nacimiento"	TEXT NOT NULL,
-	"Contraseña"	TEXT NOT NULL,
-	"Correo"	TEXT NOT NULL,
-	"Apellido_materno"	TEXT NOT NULL,
-	"Apellido_paterno"	TEXT NOT NULL,
-	"Genero"	TEXT NOT NULL,
-	"Telefono"	TEXT NOT NULL,
-	"Foto"	TEXT NOT NULL,
-	"Status"	INTEGER NOT NULL DEFAULT 0,
-	PRIMARY KEY("id_usuario")
-    '''
+    contrasena = models.CharField(max_length=100)
+    correo = models.CharField(max_length=200)
+    apellido_p = models.CharField(max_length=100)
+    apellido_m = models.CharField(max_length=100)
+    genero = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=200)
+    foto = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+
+
+class Cancion(models.Model):
+    nombre = models.CharField(max_length=100)
+    duracion = models.CharField(max_length=50)
+    genero_mus = models.CharField(max_length=100)
+    ritmo = models.CharField(max_length=100)
+    dificultad = models.CharField(max_length=100)
+
+
+class Historial_partida(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_cancion = models.ForeignKey(Cancion, on_delete=models.CASCADE)
+    puntos = models.PositiveIntegerField()  # desde 0 a 2147483647
+    fecha = models.DateTimeField()
+    tiempo_jugado = models.PositiveIntegerField()
