5a5d423be50a59f318e2cb03d3c438d098fbd2f1
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-03-30 19:05:56 -0600

    se crearon los perfiles de administradores

--- website/models.py
+++ website/models.py
@@ -18,19 +18,32 @@ class Usuario(models.Model):
     telefono = models.CharField(max_length=200)
     foto = models.CharField(max_length=100)
     status = models.BooleanField(default=False)
-
+    
+    # función para impresión
+    def __str__(self):
+        return self.nombre_usuario
 
 class Cancion(models.Model):
+    # tabla de canción
     nombre = models.CharField(max_length=100)
     duracion = models.CharField(max_length=50)
     genero_mus = models.CharField(max_length=100)
     ritmo = models.CharField(max_length=100)
     dificultad = models.CharField(max_length=100)
+    
+    # función para impresión
+    def __str__(self):
+        return self.nombre
 
 
 class Historial_partida(models.Model):
+    # tabla del historial por partida
     id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     id_cancion = models.ForeignKey(Cancion, on_delete=models.CASCADE)
     puntos = models.PositiveIntegerField()  # desde 0 a 2147483647
     fecha = models.DateTimeField()
     tiempo_jugado = models.PositiveIntegerField()
+    
+    # función para impresión
+    def __str__(self):
+        return self.puntos
